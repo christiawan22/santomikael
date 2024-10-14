27eb32764fd7c454563ed07003e3d669e44c6769
Yohanes Christiawan Atmaja <90138117+christiawan22@users.noreply.github.com>
2024-10-14 15:28:36 +0700

    Update script.blade.php

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,9 +1,9 @@
 <!-- JAVASCRIPT FILES -->
-<script src="./js/jquery.min.js'"></script>
-<script src="./js/bootstrap.bundle.min.js"></script>
-<script src="./js/jquery.sticky.js"></script>
-<script src="./js/aos.js"></script>
-<script src="./js/jquery.magnific-popup.min.js"></script>
-<script src="./js/magnific-popup-options.js"></script>
-<script src="./js/scrollspy.min.js"></script>
-<script src="./js/custom.js"></script>
+<script src="./public/js/jquery.min.js'"></script>
+<script src="./public/js/bootstrap.bundle.min.js"></script>
+<script src="./public/js/jquery.sticky.js"></script>
+<script src="./public/js/aos.js"></script>
+<script src="./public/js/jquery.magnific-popup.min.js"></script>
+<script src="./public/js/magnific-popup-options.js"></script>
+<script src="./public/js/scrollspy.min.js"></script>
+<script src="./public/js/custom.js"></script>
